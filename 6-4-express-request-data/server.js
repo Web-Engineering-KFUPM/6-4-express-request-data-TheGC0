6e8a23fb2f1b288d27b419aa1cf3ac41fc1b84be
AliShamah <ali_shamah@outlook.com>
2026-04-08 11:48:30 +0300

    add userId route parameter middleware and endpoint to return userId

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -142,10 +142,29 @@ app.get("/profile/:first/:last", (req, res) => {
 // Route params: /profile/First/Last
 
 
+app.param("userId", (req, res, next, userId) => {
+  const userIdNum = Number(userId);
+
+  if (!Number.isFinite(userIdNum) || userIdNum <= 0) {
+    return res.status(400).json({
+      ok: false,
+      error: "userId must be positive number",
+    });
+  }
+
+  req.userIdNum = userIdNum;
+  next();
+});
+
 // Route param middleware example: /users/42
 
+app.get("/users/:userId", (req, res) => {
+  res.json({
+    ok: true,
+    userId: req.userIdNum,
+  });
+});
 
 // Route params: /users/:userId route
-
 
 app.listen(3000, () => console.log("API running at http://localhost:3000"));
